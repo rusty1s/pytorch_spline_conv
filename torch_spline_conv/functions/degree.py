7e5277b1e84b2f6182d3c6ebab461118c3865b94
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-25 09:06:04 +0200

    typo

--- torch_spline_conv/functions/degree.py
+++ torch_spline_conv/functions/degree.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def node_degree(edge_index, n, out=None):
-    zero = torch.zeros(n, out=out)
+def node_degree(edge_index, num_nodes, out=None):
+    zero = torch.zeros(num_nodes, out=out)
     one = torch.ones(edge_index.size(1), out=zero.new())
     return zero.scatter_add_(0, edge_index[0], one)
