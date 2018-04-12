f22080251406982987d4c32b15d7561e6f45bdee
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-12 16:15:39 +0200

    0.3.0 fix

--- torch_spline_conv/utils/degree.py
+++ torch_spline_conv/utils/degree.py
@@ -4,6 +4,11 @@ from .new import new
 
 
 def node_degree(index, n, out=None):
-    zero = torch.zeros(n) if out is None else out.resize_(n).fill_(0)
+    if out is None:
+        zero = torch.zeros(n)
+    else:
+        out.resize_(n) if torch.is_tensor(out) else out.data.resize_(n)
+        zero = out.fill_(0)
+
     one = new(zero, index.size(0)).fill_(1)
     return zero.scatter_add_(0, index, one)
