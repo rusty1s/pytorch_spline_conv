8d6a32c41da5925b5fc9b118c14ada181b153d28
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-27 21:19:08 +0200

    version bump

--- torch_spline_conv/__init__.py
+++ torch_spline_conv/__init__.py
@@ -1,5 +1,7 @@
+from .basis import SplineBasis
+from .weighting import SplineWeighting
 from .conv import SplineConv
 
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 
-__all__ = ['SplineConv', '__version__']
+__all__ = ['SplineBasis', 'SplineWeighting', 'SplineConv', '__version__']
