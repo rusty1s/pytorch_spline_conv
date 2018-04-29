c7bef763347d76d7e7e49b32325e40e737c8a847
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-29 19:48:44 +0200

    bugfix

--- torch_spline_conv/utils/degree.py
+++ torch_spline_conv/utils/degree.py
@@ -2,6 +2,6 @@ import torch
 
 
 def degree(index, num_nodes=None, dtype=None, device=None):
-    num_nodes = index.max() + 1 if num_nodes is None else num_nodes
+    num_nodes = index.max().item() + 1 if num_nodes is None else num_nodes
     out = torch.zeros((num_nodes), dtype=dtype, device=device)
     return out.scatter_add_(0, index, out.new_ones((index.size(0))))
