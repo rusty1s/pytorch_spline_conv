2f6a99474a9c78085a78d99f25873c822eb30383
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-26 01:22:07 +0200

    [skip ci] conda

--- conda/disk.ps1
+++
@@ -1,7 +0,0 @@
-$path = "$Env:Programfiles"
-    $colItems = Get-ChildItem $path | Where-Object {$_.PSIsContainer -eq $true} | Sort-Object
-    foreach ($i in $colItems)
-    {
-        $subFolderItems = Get-ChildItem $i.FullName -recurse -force | Where-Object {$_.PSIsContainer -eq $false} | Measure-Object -property Length -sum | Select-Object Sum
-        $i.FullName + " -- " + "{0:N2}" -f ($subFolderItems.sum / 1MB) + " MB"
-    }
