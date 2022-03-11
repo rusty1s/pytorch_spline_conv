67d6316784b900663c1476d09cb9d61dc7de77f5
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-11 12:52:05 +0100

    fix version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from torch.__config__ import parallel_info
 from torch.utils.cpp_extension import (CUDA_HOME, BuildExtension, CppExtension,
                                        CUDAExtension)
 
-__version__ = '1.2.1',
+__version__ = '1.2.1'
 URL = 'https://github.com/rusty1s/pytorch_spline_conv'
 
 WITH_CUDA = torch.cuda.is_available() and CUDA_HOME is not None
