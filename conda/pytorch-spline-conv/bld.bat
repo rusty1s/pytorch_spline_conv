f94447ddd4ad6d327e648af9004f55e3a787b4f1
rusty1s <matthias.fey@tu-dortmund.de>
2022-11-14 06:46:06 +0000

    update

--- conda/pytorch-spline-conv/bld.bat
+++ conda/pytorch-spline-conv/bld.bat
@@ -1,5 +1,5 @@
 echo "=================================="
 echo "===RUNNING PIP INSTALL ==========="
-"%PYTHON%" -m pip install --verbose .
+"%PYTHON%" setup.py install
 echo "=================================="
 if errorlevel 1 exit 1
