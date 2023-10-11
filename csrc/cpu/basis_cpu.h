1c5a8987c6f2e6fd366d4c95b85cabe6d3d1bf20
rusty1s <matthias.fey@tu-dortmund.de>
2023-10-11 12:14:58 +0000

    update

--- csrc/cpu/basis_cpu.h
+++ csrc/cpu/basis_cpu.h
@@ -2,9 +2,9 @@
 
 #include <torch/extension.h>
 
-#if defined(__x86_64__)
+#if defined(__x86_64__) && __x86_64__
 __asm__(".symver pow,pow@GLIBC_2.2.5");
-#elif defined(__aarch64__)
+#elif defined(__aarch64__) && __aarch64__
 __asm__(".symver pow,pow@GLIBC_2.17");
 #endif
 
