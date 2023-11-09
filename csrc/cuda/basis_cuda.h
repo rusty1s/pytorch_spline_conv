abbb88db2b99b8fd0cbbcd766163fd1ea2eb7e1b
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-11-09 16:02:50 +0100

    Support for older `GLIBC` versions (#47)

--- csrc/cuda/basis_cuda.h
+++ csrc/cuda/basis_cuda.h
@@ -2,6 +2,10 @@
 
 #include <torch/extension.h>
 
+#if defined(__linux__) && defined(__x86_64__)
+__asm__(".symver powf,powf@GLIBC_2.2.5");
+#endif
+
 std::tuple<torch::Tensor, torch::Tensor>
 spline_basis_fw_cuda(torch::Tensor pseudo, torch::Tensor kernel_size,
                      torch::Tensor is_open_spline, int64_t degree);
