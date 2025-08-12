a80c34c7da96801edea12b29655b93cfa2e51ad5
Akihiro Nitta <akihiro@kumo.ai>
2025-08-12 13:53:53 +0100

    Support PyTorch 2.8 (#61)
    Part of https://github.com/pyg-team/pytorch_geometric/issues/10398.

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ setup(
         'graph-neural-networks',
         'spline-cnn',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=install_requires,
     extras_require={
         'test': test_requires,
