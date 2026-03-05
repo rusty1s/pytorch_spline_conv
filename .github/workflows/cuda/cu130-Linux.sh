844216c1486e11086dd9501e907392990a4fbd7b
rusty1s <matthias.fey@tu-dortmund.de>
2026-03-05 19:19:35 +0000

    update

--- .github/workflows/cuda/cu130-Linux.sh
+++ .github/workflows/cuda/cu130-Linux.sh
@@ -5,9 +5,9 @@ OS=ubuntu2204
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
-wget -nv https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-${OS}-13-0-local_13.0-580.65.06-1_amd64.deb
+wget -nv https://developer.download.nvidia.com/compute/cuda/13.0.2/local_installers/cuda-repo-${OS}-13-0-local_13.0.2-580.95.05-1_amd64.deb
 
-sudo dpkg -i cuda-repo-${OS}-13-0-local_13.0-580.65.06-1_amd64.deb
+sudo dpkg -i cuda-repo-${OS}-13-0-local_13.0.2-580.95.05-1_amd64.deb
 sudo cp /var/cuda-repo-${OS}-13-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
 sudo apt-get -qq update
