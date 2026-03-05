1e5ca9dd9cc74ba13fac5806c8a21b58db8bd325
rusty1s <matthias.fey@tu-dortmund.de>
2026-03-05 20:42:38 +0000

    update

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ def get_extensions():
             nvcc_flags = os.getenv('NVCC_FLAGS', '')
             nvcc_flags = [] if nvcc_flags == '' else nvcc_flags.split(' ')
             nvcc_flags += ['-O2']
+            nvcc_flags += ['-DTORCH_INDUCTOR_CPP_WRAPPER']
             extra_compile_args['nvcc'] = nvcc_flags
             if torch.version.hip:
                 # USE_ROCM was added to later versions of PyTorch
