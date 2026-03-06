2c6b698174a9d701b4b054a40e94d06eda809165
rusty1s <matthias.fey@tu-dortmund.de>
2026-03-06 07:35:25 +0000

    update

--- .github/workflows/cuda/cu130-Windows-env.sh
+++ .github/workflows/cuda/cu130-Windows-env.sh
@@ -2,7 +2,7 @@
 
 CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v13.0
 PATH=${CUDA_HOME}/bin:$PATH
-PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+# PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
 
 export FORCE_CUDA=1
 export TORCH_CUDA_ARCH_LIST="7.5+PTX"
