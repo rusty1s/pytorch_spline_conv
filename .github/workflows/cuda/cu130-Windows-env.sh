79c934446d269b57f1b599bd0c8d41c95db0cdfe
rusty1s <matthias.fey@tu-dortmund.de>
2026-03-06 13:05:25 +0000

    Set DISTUTILS_USE_SDK=1 for CUDA 13.0 Windows builds
    Required when the MSVC dev environment is pre-activated to prevent
    PyTorch's cpp_extension from raising an error about multiple VC
    environment activations.

--- .github/workflows/cuda/cu130-Windows-env.sh
+++ .github/workflows/cuda/cu130-Windows-env.sh
@@ -5,4 +5,5 @@ PATH=${CUDA_HOME}/bin:$PATH
 # PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
 
 export FORCE_CUDA=1
+export DISTUTILS_USE_SDK=1
 export TORCH_CUDA_ARCH_LIST="7.5+PTX"
