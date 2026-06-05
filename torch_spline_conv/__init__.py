df9394abafa3cd7a9e670bb8693317c26a5901b6
Akihiro Nitta <akihiro@kumo.ai>
2026-06-05 10:21:12 -0700

    Update torch_spline_conv/__init__.py

--- torch_spline_conv/__init__.py
+++ torch_spline_conv/__init__.py
@@ -8,7 +8,7 @@ __version__ = '1.2.2'
 
 warnings.warn(
     "'torch-spline-conv' is deprecated and no longer maintained. All "
-    "functionality has been migrated to 'pyg-lib' "
+    "functionality has been migrated to 'pyg-lib>=0.7.0' "
     "(https://github.com/pyg-team/pyg-lib).",
     DeprecationWarning,
     stacklevel=2,
